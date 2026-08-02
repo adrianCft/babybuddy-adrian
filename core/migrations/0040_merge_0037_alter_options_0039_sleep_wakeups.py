e8c3264ca521732a4369ea69897a7c99708a70b8
Adrián Clavero Sanz <aclavero@unedbarbastro.es>
2026-08-02 21:45:50 +0200

    Compatibilizar migraciones con la instalación existente

---
+++ core/migrations/0040_merge_0037_alter_options_0039_sleep_wakeups.py
@@ -0,0 +1,14 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        (
+            "core",
+            "0037_alter_bmi_options_alter_headcircumference_options_and_more",
+        ),
+        ("core", "0039_sleep_wakeups"),
+    ]
+
+    operations = []
